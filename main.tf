5c53a8168d95c403e76bbc4e4bd98e56832cbaf4
wallacepf <wallacepf@me.com>
2023-06-29 15:38:27 -0300

    VPC Name Changing on module

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.77.0"
 
-  name                 = "education"
+  name                 = "nocode-education"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
